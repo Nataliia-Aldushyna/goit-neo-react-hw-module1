2fff01c295174052c1499c680ca47c39ab3179fa
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-11-15 22:12:53 +0100

    updated task-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Profile from './Profile';
-import FriendList from './FriendList';
+import Profile from './components/Profile';
+import FriendList from './components/FriendList';
 import userData from './userData.json';
 import friends from './friends.json';
 
